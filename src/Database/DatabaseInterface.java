7368390db74cf43884198a6fa607cc80a7a8e807
Juliano Zatta <julianokzatta@gmail.com>
2014-12-09 03:13:29 -0200

    Started load from database

--- src/Database/DatabaseInterface.java
+++ src/Database/DatabaseInterface.java
@@ -1,5 +1,10 @@
 package Database;
 
 interface DatabaseInterface{
-	
+	// Database.Sale completely different of background.Sale
+	Sale[] loadSales(Timestamp from, Timestamp to, String sellerName, String enterpriseName, String boatName, String boatEnterpriseName);
+	void storeSale(Sale sale);
+	// Database.Tour completely different of background.Tour
+	Tour[] loadTours(Timestamp from, Timestamp to, String boatName, String boatEnterpriseName);
+	void storeTours(Tour tour);
 }
--- src/Database/SQLDatabase.java
+++ src/Database/SQLDatabase.java
@@ -1,5 +1,120 @@
 package Database;
 
 class SQLDatabase implements DatabaseInterface{
+	private String hostname;
+	private String username;
+	private String password;
 	
+	public SQLDatabase(String hostname, String username, String password) {
+		this.hostname = hostname;
+		this.username = username;
+		this.password = password;
+	}
+	
+	private Connection connectServer() throws SQLException, ClassNotFoundException{
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://"+this.hostname+"/"+database,this.username,this.password);
+	}
+	
+	private ResultSet getData(String rule) throws SQLException{
+		return connectServer().createStatement().executeQuery(rule);
+	}
+	
+	private void createIfNoExist(){
+		
+	}
+	
+	public Sale[] loadSales(Timestamp from, Timestamp to, String sellerName, String enterpriseName, String boatName, String boatEnterpriseName){
+		// select all(colums) from (table) where
+		String rule = "SELECT * FROM Sales WHERE";
+		boolean runCommand = false
+		if (from != null){
+			rule += " departure>="+from;
+			runCommand = true;
+		}
+		if (to != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " departure<="+to;
+			runCommand = true;
+		}
+		if (sellerName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " sellerName='"+sellerName+"'";
+			runCommand = true;
+		}
+		if (enterpriseName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " enterpriseName<='"+enterpriseName+"'";
+			runCommand = true;
+		}
+		if (boatName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " boatName<='"+boatName+"'";
+			runCommand = true;
+		}
+		if (boatEnterpriseName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " boatEnterpriseName<='"+boatEnterpriseName+"'";
+			runCommand = true;
+		}
+		if (!runCommand){
+			rule = "SELECT * FROM Sales";
+		}
+		rule += ";";
+		ResultSet result = getData(rule);
+		
+		// process data
+		
+		return null;
+	}
+	
+	Tour[] loadTours(Timestamp from, Timestamp to, String boatName, String boatEnterpriseName){
+		// select all(colums) from (table) where
+		String rule = "SELECT * FROM Tour WHERE";
+		boolean runCommand = false
+		if (from != null){
+			rule += " departure>="+from;
+			runCommand = true;
+		}
+		if (to != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " departure<="+to;
+			runCommand = true;
+		}
+		if (boatName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " boatName<='"+boatName+"'";
+			runCommand = true;
+		}
+		if (boatEnterpriseName != null){
+			if (runCommand){
+				rule += " AND";
+			}
+			rule += " boatEnterpriseName<='"+boatEnterpriseName+"'";
+			runCommand = true;
+		}
+		if (!runCommand){
+			rule = "SELECT * FROM Tour";
+		}
+		rule += ";";
+		ResultSet result = getData(rule);
+		
+		// process data
+		
+		return null;
+	}
 }
