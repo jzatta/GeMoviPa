6a74b3a9e6356c0fce95d231b3ab2cb209b09a49
Juliano Zatta <julianokzatta@gmail.com>
2014-12-13 20:15:23 -0200

    I dont dnow what i do here, i think nothing

--- src/Database/DatabaseInterface.java
+++ src/Database/DatabaseInterface.java
@@ -5,10 +5,12 @@ import java.sql.*;
 import java.util.List;
 
 public interface DatabaseInterface{
-  // Database.Sale completely different of background.Sale
   List<Sale> loadSales(Timestamp from, Timestamp to, String sellerName, String enterpriseName, String boatName, String boatEnterpriseName);
   void storeSale(Sale sale);
-  // Database.Tour completely different of background.Tour
   List<Tour> loadTours(Timestamp from, Timestamp to, String boatName, String boatEnterpriseName);
   void storeTour(Tour tour);
+  List<Boat> loadBoats(String boatName, String boatEnterprise);
+  void storeBoat(Boat boat);
+  List<Enterprise> loadBoats(String enterpriseName);
+  void storeEnterprise(Enterprise enterprise);
 }
